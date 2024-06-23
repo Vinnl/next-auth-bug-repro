7bedfc1659cd5527ad727be7a9be828fe2c56507
Vincent <git@vincenttunru.com>
2024-06-23 20:24:30 +0200

    Add JWT-based refresh token rotation for Spotify
    This reproduces a bug: if you sign in, wait two minutes (the
    amount of time I set it up to try to refresh the access token),
    then refresh the page, it will try to rotate the refresh token
    twice, which will result in an error:
    
    > Could not refresh access token:
    > { error: 'invalid_grant', error_description: 'Refresh token revoked' }
    
    This is because the first (successful) attempt will invalidate that
    refresh token.

--- auth.ts
+++ auth.ts
@@ -1,39 +1,40 @@
 import NextAuth from "next-auth"
 import "next-auth/jwt"
 
-import Apple from "next-auth/providers/apple"
-import Auth0 from "next-auth/providers/auth0"
-import AzureB2C from "next-auth/providers/azure-ad-b2c"
-import BankIDNorway from "next-auth/providers/bankid-no"
-import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
-import Cognito from "next-auth/providers/cognito"
-import Coinbase from "next-auth/providers/coinbase"
-import Discord from "next-auth/providers/discord"
-import Dropbox from "next-auth/providers/dropbox"
-import Facebook from "next-auth/providers/facebook"
-import GitHub from "next-auth/providers/github"
-import GitLab from "next-auth/providers/gitlab"
-import Google from "next-auth/providers/google"
-import Hubspot from "next-auth/providers/hubspot"
-import Keycloak from "next-auth/providers/keycloak"
-import LinkedIn from "next-auth/providers/linkedin"
-import Netlify from "next-auth/providers/netlify"
-import Okta from "next-auth/providers/okta"
-import Passage from "next-auth/providers/passage"
-import Passkey from "next-auth/providers/passkey"
-import Pinterest from "next-auth/providers/pinterest"
-import Reddit from "next-auth/providers/reddit"
-import Slack from "next-auth/providers/slack"
+// import Apple from "next-auth/providers/apple"
+// import Auth0 from "next-auth/providers/auth0"
+// import AzureB2C from "next-auth/providers/azure-ad-b2c"
+// import BankIDNorway from "next-auth/providers/bankid-no"
+// import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
+// import Cognito from "next-auth/providers/cognito"
+// import Coinbase from "next-auth/providers/coinbase"
+// import Discord from "next-auth/providers/discord"
+// import Dropbox from "next-auth/providers/dropbox"
+// import Facebook from "next-auth/providers/facebook"
+// import GitHub from "next-auth/providers/github"
+// import GitLab from "next-auth/providers/gitlab"
+// import Google from "next-auth/providers/google"
+// import Hubspot from "next-auth/providers/hubspot"
+// import Keycloak from "next-auth/providers/keycloak"
+// import LinkedIn from "next-auth/providers/linkedin"
+// import Netlify from "next-auth/providers/netlify"
+// import Okta from "next-auth/providers/okta"
+// import Passage from "next-auth/providers/passage"
+// import Passkey from "next-auth/providers/passkey"
+// import Pinterest from "next-auth/providers/pinterest"
+// import Reddit from "next-auth/providers/reddit"
+// import Slack from "next-auth/providers/slack"
 import Spotify from "next-auth/providers/spotify"
-import Twitch from "next-auth/providers/twitch"
-import Twitter from "next-auth/providers/twitter"
-import WorkOS from "next-auth/providers/workos"
-import Zoom from "next-auth/providers/zoom"
+// import Twitch from "next-auth/providers/twitch"
+// import Twitter from "next-auth/providers/twitter"
+// import WorkOS from "next-auth/providers/workos"
+// import Zoom from "next-auth/providers/zoom"
 import { createStorage } from "unstorage"
 import memoryDriver from "unstorage/drivers/memory"
 import vercelKVDriver from "unstorage/drivers/vercel-kv"
 import { UnstorageAdapter } from "@auth/unstorage-adapter"
-import type { NextAuthConfig } from "next-auth"
+import type { Account, NextAuthConfig } from "next-auth"
+import { JWT } from "next-auth/jwt"
 
 const storage = createStorage({
   driver: process.env.VERCEL
@@ -47,54 +48,54 @@ const storage = createStorage({
 
 const config = {
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
-  adapter: UnstorageAdapter(storage),
+  // adapter: UnstorageAdapter(storage),
   providers: [
-    Apple,
-    Auth0,
-    AzureB2C({
-      clientId: process.env.AUTH_AZURE_AD_B2C_ID,
-      clientSecret: process.env.AUTH_AZURE_AD_B2C_SECRET,
-      issuer: process.env.AUTH_AZURE_AD_B2C_ISSUER,
-    }),
-    BankIDNorway,
-    BoxyHQSAML({
-      clientId: "dummy",
-      clientSecret: "dummy",
-      issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
-    }),
-    Cognito,
-    Coinbase,
-    Discord,
-    Dropbox,
-    Facebook,
-    GitHub,
-    GitLab,
-    Google,
-    Hubspot,
-    Keycloak({ name: "Keycloak (bob/bob)" }),
-    LinkedIn,
-    Netlify,
-    Okta,
-    Passkey({
-      formFields: {
-        email: {
-          label: "Username",
-          required: true,
-          autocomplete: "username webauthn",
-        },
-      },
-    }),
-    Passage,
-    Pinterest,
-    Reddit,
-    Slack,
+    // Apple,
+    // Auth0,
+    // AzureB2C({
+    //   clientId: process.env.AUTH_AZURE_AD_B2C_ID,
+    //   clientSecret: process.env.AUTH_AZURE_AD_B2C_SECRET,
+    //   issuer: process.env.AUTH_AZURE_AD_B2C_ISSUER,
+    // }),
+    // BankIDNorway,
+    // BoxyHQSAML({
+    //   clientId: "dummy",
+    //   clientSecret: "dummy",
+    //   issuer: process.env.AUTH_BOXYHQ_SAML_ISSUER,
+    // }),
+    // Cognito,
+    // Coinbase,
+    // Discord,
+    // Dropbox,
+    // Facebook,
+    // GitHub,
+    // GitLab,
+    // Google,
+    // Hubspot,
+    // Keycloak({ name: "Keycloak (bob/bob)" }),
+    // LinkedIn,
+    // Netlify,
+    // Okta,
+    // Passkey({
+    //   formFields: {
+    //     email: {
+    //       label: "Username",
+    //       required: true,
+    //       autocomplete: "username webauthn",
+    //     },
+    //   },
+    // }),
+    // Passage,
+    // Pinterest,
+    // Reddit,
+    // Slack,
     Spotify,
-    Twitch,
-    Twitter,
-    WorkOS({
-      connection: process.env.AUTH_WORKOS_CONNECTION!,
-    }),
-    Zoom,
+    // Twitch,
+    // Twitter,
+    // WorkOS({
+    //   connection: process.env.AUTH_WORKOS_CONNECTION!,
+    // }),
+    // Zoom,
   ],
   basePath: "/auth",
   callbacks: {
@@ -103,16 +104,60 @@ const config = {
       if (pathname === "/middleware-example") return !!auth
       return true
     },
-    jwt({ token, trigger, session, account }) {
+    async jwt({ token, trigger, session, account }) {
       if (trigger === "update") token.name = session.user.name
       if (account?.provider === "keycloak") {
         return { ...token, accessToken: account.access_token }
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+      } else {
+        if (
+          typeof token.expiresAt === "number" &&
+          typeof token.refreshToken === "string" &&
+          // Already refreshing the access token after two minutes to make
+          // replication easier:
+          Date.now() > ((token.expiresAt * 1000) - (58 * 60 * 1000))
+        ) {
+          const requestTime = Date.now();
+          try {
+            const response = await fetch(
+              "https://accounts.spotify.com/api/token",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: new URLSearchParams({
+                  grant_type: "refresh_token",
+                  refresh_token: token.refreshToken,
+                  client_id: process.env.AUTH_SPOTIFY_ID!,
+                }),
+              },
+            );
+            const data = await response.json();
+            if (!response.ok) {
+              throw data;
+            }
+            console.log("Refreshed token:", data);
+            token.accessToken = data.access_token;
+            token.refreshToken = data.refresh_token;
+            token.expiresAt = requestTime + data.expires_in * 1000;
+          } catch (e) {
+            console.log("Could not refresh access token:", e);
+            token.error = "RefreshAccessTokenError";
+          }
+        }
       }
       return token
     },
     async session({ session, token }) {
       if (token?.accessToken) {
         session.accessToken = token.accessToken
+        session.expiresAt = token.expiresAt;
+        session.error = token.error;
       }
       return session
     },
@@ -128,11 +173,16 @@ export const { handlers, auth, signIn, signOut } = NextAuth(config)
 declare module "next-auth" {
   interface Session {
     accessToken?: string
+    expiresAt: JWT["expiresAt"] | null;
+    error?: "RefreshAccessTokenError";
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     accessToken?: string
+    refreshToken: Account["refresh_token"];
+    expiresAt: Account["expires_at"];
+    error?: "RefreshAccessTokenError";
   }
 }
